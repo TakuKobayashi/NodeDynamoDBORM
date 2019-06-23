d84f1f37b755cbf9c081777173f4fe300eddb15d
Taku Kobayashi <keep_slimbody@yahoo.co.jp>
2019-06-23 09:33:30 +0900

    add create table and delete table test case

--- lib/dynamodb-orm.spec.ts
+++ lib/dynamodb-orm.spec.ts
@@ -1,4 +1,48 @@
-const DynamoORM = require('./dynamoorm');
+import DynamoDBORM from './dynamodb-orm';
+
+const AWS = require('aws-sdk');
+
+beforeAll(async () => {
+  AWS.config.update({ region: 'ap-northeast-1' });
+  const dynamodb = new AWS.DynamoDB({ endpoint: new AWS.Endpoint('http://localhost:8000') });
+  const params = {
+    AttributeDefinitions: [
+      {
+        AttributeName: 'Artist',
+        AttributeType: 'S',
+      },
+      {
+        AttributeName: 'SongTitle',
+        AttributeType: 'S',
+      },
+    ],
+    KeySchema: [
+      {
+        AttributeName: 'Artist',
+        KeyType: 'HASH',
+      },
+      {
+        AttributeName: 'SongTitle',
+        KeyType: 'RANGE',
+      },
+    ],
+    ProvisionedThroughput: {
+      ReadCapacityUnits: 5,
+      WriteCapacityUnits: 5,
+    },
+    TableName: 'Music',
+  };
+  await dynamodb.createTable(params).promise();
+});
+
+afterAll(async () => {
+  const dynamodb = new AWS.DynamoDB({ endpoint: new AWS.Endpoint('http://localhost:8000') });
+  await dynamodb
+    .deleteTable({
+      TableName: 'Music',
+    })
+    .promise();
+});
 
 describe('DynamoORM', () => {
   it.skip('updateConfig', async () => {});
